a227d543bf67e57cfccf4cae7e2dddead6203e06
George Chang <su3a8a82k7@gmail.com>
2020-04-18 14:39:40 +0700

    Add US part

--- driving.py
+++ driving.py
@@ -6,5 +6,11 @@ if country == '台灣':
 	if age >= 18:
 		print('你可以考駕照')
 	else:
-		print('你還無法考駕照')	
+		print('你還無法考駕照')
+elif country == '美國':
+	if age >= 16:
+		print('你可以考駕照')
+	else:
+		print('你還無法考駕照')
+						
 
